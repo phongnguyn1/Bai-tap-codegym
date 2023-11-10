2257667264146a51da1b1427a12f3d4b5386d934
phongnguyn1 <phongdinh250703@gmail.com>
2023-11-10 15:13:11 +0700

    11102

---
+++ 11102.js
@@ -0,0 +1,13 @@
+let trinhduyet = prompt("xin cai trinh duyet ");
+switch (trinhduyet) {
+    case 'chrome' :
+alert("OK DUOC ") ;
+break ;
+    case 'firefox' :
+    case 'Edge' :
+    case 'opera ' :
+        alert("cung duoc luon ") ;
+        break ;
+    default:
+        alert('Hi vong duoc');
+}
