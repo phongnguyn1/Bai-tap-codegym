1ad6f3e1788b4de1885449e0ab0b86fd968de059
phongnguyn1 <phongdinh250703@gmail.com>
2023-11-10 14:57:08 +0700

    11102

---
+++ 1110.js
@@ -0,0 +1,11 @@
+let taikhoan= prompt("nhap tai khoan");
+if (taikhoan == "admin" ) {
+    let matkhau = prompt("nhap mk");
+    if (matkhau =="Themaster") {
+        alert("dung roi");
+    }else{
+        alert("sai mk") ;
+    }
+  }else{
+    alert("sai ten ")
+}
