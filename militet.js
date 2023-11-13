f517bf8c42d9ac00d63e8f79f9a5c926ef98faa4
phongnguyn1 <phongdinh250703@gmail.com>
2023-11-13 09:19:30 +0700

    militet1

---
+++ militet.js
@@ -0,0 +1,6 @@
+inputWidth = prompt("Enter the width");
+let width = parseInt(inputWidth);
+let area = width * width;
+let parameter = width + width + width + width
+document.write("The area is: " + area);
+document.write("The parameter is: " + parameter);
---
+++ militet3b.js
@@ -0,0 +1,10 @@
+inputA = prompt("nhap so a") ;
+inputB = prompt("nhap so B ");
+let A = parseInt( inputA)
+let B = parseInt(inputB)
+let Total = A+B
+let power2 = A * A
+let powerB = A * B
+document.write("Total is " + Total);
+document.write(" A power by 2 is: " + power2 );
+document.write(" A power by B is: " + powerB );
